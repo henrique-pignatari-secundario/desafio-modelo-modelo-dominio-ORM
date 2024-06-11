c68fe5483f7a498162174bbbe293bd9a6b2c0728
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2024-06-11 14:14:12 -0300

    feat: initial commit

---
+++ src/main/java/com/henrique/desafio_modelo_dominio_ORM/DesafioModeloDominioOrmApplication.java
@@ -0,0 +1,13 @@
+package com.henrique.desafio_modelo_dominio_ORM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioModeloDominioOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioModeloDominioOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henrique/desafio_modelo_dominio_ORM/DesafioModeloDominioOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henrique.desafio_modelo_dominio_ORM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioModeloDominioOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
